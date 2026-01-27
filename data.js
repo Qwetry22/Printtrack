2b40a37684a31ace8b0e432f797611354f950eae
Qwetry22 <jedrzejsmura@gmail.com>
2026-01-27 21:28:04 +0100

    Add orders array with initial order data

---
+++ data.js
@@ -0,0 +1,14 @@
+const orders = [
+  {
+    code: "PT-0001",
+    status: "Drukuje się",
+    step: "2/5",
+    comment: "PLA czarne, 0.2mm"
+  },
+  {
+    code: "PT-0002",
+    status: "Gotowe do odbioru",
+    step: "5/5",
+    comment: "PETG czerwony"
+  }
+];
